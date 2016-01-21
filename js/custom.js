9eb97750129e97695d5596406a43301011d2e078
viomac <vmaceda_29@hotmail.com>
2016-01-20 22:04:46 -0600

    Updated modal content.

--- js/custom.js
+++ js/custom.js
@@ -16,4 +16,12 @@ $('.category[category=all]').click(function() {
 });
 
 
+// SERVICE CARD MODAL
+
+$('.service-card-btn').click(function() {
+    caption = $(this).parents('.thumbnail');
+    $('#service-card-modal .service-label').text($(caption).find('.service-name').text());
+    $('#service-card-modal .provider-label').text($(caption).find('.provider-name').text()+" /");
+    $('#service-card-modal .price-label').text($(caption).find('.price').text());
+});
 
